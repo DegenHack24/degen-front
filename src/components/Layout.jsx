6c5e2085d6ccb55fe6ba1c0193d198de28d0e777
rozekmichal <michalrozek90@gmail.com>
2024-02-17 12:20:12 +0100

    routing

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,25 +1,33 @@
+import { useEffect } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import { useMetaMaskContext } from "../utils/contexts/metamaskContext";
 import AssetsTable from "./AssetsTable";
 import Dashboard from "./Dashboard";
+import Navbar from "./Navbar";
 
 export default function Layout() {
+  const navigate = useNavigate();
+
   const { buttonText, connectWalletHandler, metaMaskAccount } =
     useMetaMaskContext();
 
+  useEffect(() => {
+    if (metaMaskAccount) navigate("/assets-table");
+    else navigate("/");
+  }, [metaMaskAccount]);
+
   return (
     <div className="w-full justify-center mx-auto">
-      <div className="flex flex-row gap-10">
-        <div>BULLetproof</div>
-        <button
-          disabled={buttonText === "Connected"}
-          onClick={connectWalletHandler}
-        >
-          <div className="w-20">{buttonText}</div>
-        </button>
-      </div>
-      <div className="flex justify-center">
-        {metaMaskAccount ? <AssetsTable /> : <Dashboard />}
-      </div>
+      <Navbar
+        buttonText={buttonText}
+        connectWalletHandler={connectWalletHandler}
+      />
+      <main className="w-full items-center flex-1 min-h-screen">
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/assets-table" element={<AssetsTable />} />
+        </Routes>
+      </main>
     </div>
   );
 }
